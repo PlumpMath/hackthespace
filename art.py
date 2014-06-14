f746c2fd98454ab5d3262bcb7c4fab8347a9d21a
opyate@gmail.com <Juan Matthys Uys>
2014-06-14 12:32:18 +0100

    redis, and minimal api

--- art.py
+++ art.py
@@ -2,26 +2,50 @@ import os
 from flask import Flask
 from flask import Response
 import json
+import random
+from redis import Redis
 
 app = Flask(__name__)
-
+app.config['REDIS_DB'] = 'art'
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis = Redis.from_url(redis_url)
 links = []
 
 def getlinks():
   global links
   if not links:
     with open('images.txt') as f:
-      links = f.readlines()
-      print '[%s]' % ', '.join(map(str, links))
+      links = [line.rstrip() for line in f]
   return links
+
+def dbset():
+  redis.set(app.config['REDIS_DB'] + ':xxx', 42)
+
+def dbget():
+  return redis.get(app.config['REDIS_DB'] +':xxx')
 
 @app.route('/')
 def main():
   return 'main'
 
+@app.route('/artwork', methods=['GET'])
+def get():
+  result = dbget()
+  return Response(json.dumps(result), mimetype='application/json')
+
+@app.route('/artwork', methods=['POST'])
+def set():
+  dbset()
+  return 'OK'
+
 @app.route('/images')
 def images():
-  return Response(json.dumps(getlinks()),  mimetype='application/json')
+  return Response(json.dumps(getlinks()), mimetype='application/json')
+
+@app.route('/random-image')
+def randomimage():
+  n = random.randint(0, len(getlinks()))
+  return Response(json.dumps({'image':getlinks()[n]}), mimetype='application/json')
 
 if __name__ == '__main__':
   app.run(debug=True)
